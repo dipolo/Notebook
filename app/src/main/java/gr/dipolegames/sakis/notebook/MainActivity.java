786224922679712bb7c2eb332a3d64c6853713b0
sakis <petsossakis@gmail.com>
2016-07-10 00:38:49 +0300

    Commit 5 - Prefereces: Set and Retreive. Video 82

--- app/src/main/java/gr/dipolegames/sakis/notebook/MainActivity.java
+++ app/src/main/java/gr/dipolegames/sakis/notebook/MainActivity.java
@@ -1,7 +1,10 @@
 package gr.dipolegames.sakis.notebook;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +12,7 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        loadPreferences();
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -53,10 +58,11 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, AppPreferences.class);
+            startActivity(intent);
             return true;
         }
-
-        if (id == R.id.action_add_note) {
+        else if (id == R.id.action_add_note) {
             Intent intent = new Intent(this, NoteDetailActivity.class);
             intent.putExtra(MainActivity.NOTE_FRAGMENT_TO_LOAD_EXTRA, FragmentToLaunch.CREATE);
             startActivity(intent);
@@ -64,5 +70,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void loadPreferences(){
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isBackgroundDark = sharedPreferences.getBoolean("background_color", false);
+        if(isBackgroundDark){
+            LinearLayout mainLayout = (LinearLayout) findViewById(R.id.mainActivityLayout);
+            mainLayout.setBackgroundColor(Color.parseColor("#3c3f41"));
+        }
+        String notebookTitle = sharedPreferences.getString("title", "Notebook");
+        setTitle(notebookTitle);
+
     }
 }
